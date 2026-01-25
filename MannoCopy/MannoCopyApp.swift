2792df17e88c06bcb28fa7a8b59e4c9aeaa7bb02
Obst-Terminator <besser.manolito@gmail.com>
2026-01-25 07:20:00 +0100

    Initial Commit

---
+++ MannoCopy/MannoCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MannoCopyApp.swift
+//  MannoCopy
+//
+//  Created by Manolito Besser on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MannoCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
